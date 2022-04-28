1895cb960bd81209fd09dc8299f5105e5a33de42
Aryane Maria Valentim Teixeira <aryane.teixeira@nava.com.br>
2022-04-28 17:25:18 -0300

    first commit

---
+++ src/main/java/br/com/senac/SpringCamundaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamundaApplication.class, args);
+	}
+
+}
